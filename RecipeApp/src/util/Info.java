1b0461b0a83bd9a7b29759078000dfe03e44e97c
Jerry <jlg.geek@gmail.com>
2021-03-24 14:52:45 -0500

    Factored in come feedback

--- RecipeApp/src/util/Info.java
+++ RecipeApp/src/util/Info.java
@@ -23,16 +23,16 @@ public interface Info {
    out.println(".w3-bar-block .w3-bar-item {padding:20px}");
    out.println("</style>");
    out.println("<body>");
-   out.println("<nav class=\"w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left\" style=\"display:none;z-index:2;width:40%;min-width:300px\" id=\"mySidebar\">");  
+   out.println("<nav class=\"w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left\" style=\"display:none;z-index:2;width:25%;min-width:300px\" id=\"mySidebar\">");  
    out.println("<a href=\"javascript:void(0)\" onclick=\"w3_close()\" class=\"w3-bar-item w3-button\">Close Menu</a>");  
    out.println("<a href=/" + projectName + "/" + homeWebName + " onclick=\"w3_close()\" class=\"w3-bar-item w3-button\">Food</a>");  
    out.println("<a href=/" + projectName + "/" + aboutWebName + " onclick=\"w3_close()\" class=\"w3-bar-item w3-button\">About</a>");  
    out.println("</nav>");
    out.println("<div class=\"w3-top\">");
    out.println("<div class=\"w3-white w3-xlarge\" style=\"max-width:1200px;margin:auto\">");
-   out.println("<div class=\"w3-button w3-padding-16 w3-left\" onclick=\"w3_open()\">=</div>");
+   out.println("<div class=\"w3-button w3-padding-16 w3-left\" onclick=\"w3_open()\"><img src=Images/menu.jpg alt=menu style=\"width:30px\"></div>");
+   //"<img src=\""+recipe.getImage()+"\" alt=\""+recipe.getImage()+"\" style=\"width:100%\">"
    out.println("<div class=\"w3-right w3-padding-16\"><a href=/" + projectName + "/" + searchWebName + ">Search</a></div>");
-   //out.println("<div class=\"w3-right w3-padding-16\"><a href=\"/RecipeApp/SimpleRecipeSearch.html\">Search</a></div>");
    out.println("<div class=\"w3-center w3-padding-16\"><a href=\"/RecipeApp/Index\">Recipes</a></div>");
    out.println("</div>");  
    out.println("</div>");  
